3e39df1d6cbe4c3566345989eb88d494b3b96dea
Ashin Abraham <ashine@alokin.in>
2018-03-20 08:57:41 +0530

    code Update

--- website/website/settings.py
+++ website/website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'SensorDirectory',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r'C:\Users\HP\Desktop\appNew\website\template'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
