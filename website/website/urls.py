c46783802fcbbb476bf171b95f3e90502b5ee765
Ashin Abraham <ashine@alokin.in>
2018-03-20 09:12:59 +0530

    base index added

--- website/website/urls.py
+++ website/website/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('SensorDirectory.url')),
     path('admin/', admin.site.urls),
-    path('SensorDirectory/', include('SensorDirectory.url')),
 
 ]
 
